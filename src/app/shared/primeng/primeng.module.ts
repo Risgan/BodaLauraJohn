cb43798a80e7cce4ea078b5056f2ae35d6165b5d
John Rueda <risgan2727@gmail.com>
2023-06-21 18:25:22 -0500

    commit

--- src/app/shared/primeng/primeng.module.ts
+++ src/app/shared/primeng/primeng.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
 import { SidebarModule } from 'primeng/sidebar';
+import { ImageModule } from 'primeng/image';
+import { GalleriaModule } from 'primeng/galleria';
+import { CarouselModule } from 'primeng/carousel';
 
 const modules=[
   ButtonModule,
   ToastModule,
-  SidebarModule
+  SidebarModule,
+  ImageModule,
+  GalleriaModule,
+  CarouselModule
 ]
 
 @NgModule({
